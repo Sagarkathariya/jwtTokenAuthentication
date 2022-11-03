e5f8c5acfebba3a746d0b7b3636b8f3a97c424bb
Sagar Kathariya <sagarkathariya@gmail.com>
2022-11-03 18:26:58 +0545

    Add project files.

---
+++ jwtTokenAuthentication/Data/DbContextClass.cs
@@ -0,0 +1,20 @@
+﻿using jwtTokenAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace jwtTokenAuthentication.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ jwtTokenAuthentication/Models/JWTTokenResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace jwtTokenAuthentication.Models
+{
+    public class JWTTokenResponse
+    {
+        public string? Token { get; set; }
+    }
+}
